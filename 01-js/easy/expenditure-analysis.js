52eb17971e1d256a6b21a46cbb6c38abb55142ff
PSilyDev <codes.prakhar@gmail.com>
2024-05-06 15:09:35 +0530

    expenditure analysis solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -3,7 +3,7 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-        {
+  {
 		id: 1,
 		timestamp: 1656076800000,
 		price: 10,
@@ -14,7 +14,20 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let arr = []
+  let tot = {}
+  transactions.forEach(trans => {
+    if(!tot[trans.category]){
+      tot[trans.category] = 0
+    }
+    tot[trans.category] += trans.price
+  })
+  for(let category in tot){
+    obj = {category: category, totalSpent: tot[category]}
+    arr.push(obj)
+  }
+  
+  return arr;
 }
 
 module.exports = calculateTotalSpentByCategory;
